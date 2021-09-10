c9e964d0614a4dd4d63a9a42af96443450347752
RandelSouza <randelsouza88@gmail.com>
2021-09-10 15:29:04 -0300

    alert console.log strings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-alert("Olá mundo!");
+let nome = "Randel Souza";
+let idade = 25;
+let idade2 = 10;
+var frase = "Japão é o melhor time do mundo"; 
+//alert(` ${nome} tem ${idade} anos`); // Template String
+
+//lert(`${idade + idade2}`);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão", "Brasil").toLowerCase());
+
+alert(frase.replace("Brasil", "Japão").toUpperCase())
+
+// multiplicacao
+let n1 = 2;
+let n2 = 3;
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 - n2);
+console.log(n1 + n2);
