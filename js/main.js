29d619df1ade576292bb2c7289062a6d34ee9676
RandelSouza <randelsouza88@gmail.com>
2021-09-27 12:56:53 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -54,4 +54,54 @@ let frutas = [
 console.log(frutas[0].nome, frutas[0].cor);
 console.log(frutas[1].nome, frutas[1].cor);
 console.log(frutas[2].nome, frutas[2].cor);
+
+let idade = prompt("Qual a sua idade?"); // Nunca tinha usado!! Muito bom.
+
+if( idade >= 18){
+    console.log("Maior de idade");
+}else{
+    console.log("Menor de idade");
+}
+
+let count = 0;
+// Laço de repetição while
+while(count < 5){
+    console.log(count);
+    count++;
+}
+
+for(var i = 0; i <= 5; i++){
+    console.log(i);
+}
+let date = new Date();
+console.log(date.getMonth()+1);
+
+var a = ["a", "a"];
+console.log(a); 
+
+
+function validarIdade(idade){
+    if(idade >= 18) return true;
+    return false;
+}
+
+let idade = prompt("Qual a sua idade?");
+console.log(validarIdade(idade));
 */
+document.getElementById("redirecionar").onclick = redirecionar;
+document.getElementById("button").onclick = button;
+document.getElementById("change").onmouseover = change;
+
+function button(){
+    //alert("Obrigado por clicar!");
+    document.getElementById("phrase").innerHTML = "<b>Obrigrado por participar dessa aula</b>";
+}
+
+function redirecionar(){
+    //window.open("https://www.linkedin.com/in/randelsouza/");
+    window.location.href = "https://www.linkedin.com/in/randelsouza/";
+}
+
+function change(){
+    document.getElementById("phrase").innerHTML = "<b>MouseOver Funcionando!!</b>"
+}
